95f009e9b255ecac82361de831347470dea17944
ana carolina garcone <Ana Carolina>
2022-02-22 22:13:46 -0300

    inicializando express, criando o metodo get da API

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log('testando');
+import express, { Request, Response } from 'express';
+
+const server = express();
+
+server.get('/', (resquest: Request, response: Response) => {
+    return response.json({message: 'Bem vindo a minha API'})
+});
+
+server.listen(5000, () => {
+    console.log(`Servidor rodando na porta localhost:5000/`)
+});
